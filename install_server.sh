d62e3aaed877961945da6b9433f92f6de201ac77
Dexter-Hysteria <145413662+Dexter-Hysteria@users.noreply.github.com>
2023-11-07 14:14:42 +0800

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -642,6 +642,7 @@ After=network.target
 
 [Service]
 Type=simple
+ExecStart=$EXECUTABLE_INSTALL_PATH -config ${_config_name}.json server
 WorkingDirectory=$CONFIG_DIR
 User=$HYSTERIA_USER
 Group=$HYSTERIA_USER
@@ -727,7 +728,7 @@ stop_running_services() {
 ###
 # HYSTERIA & GITHUB API
 ###
-#ExecStart=$EXECUTABLE_INSTALL_PATH -config ${_config_name}.json server
+
 is_hysteria_installed() {
   # RETURN VALUE
   # 0: hysteria is installed
